d6a903414a318824fd87afa4bd63c9141a9eed10
Dilip Sundarraj <dilip.sundarraj@target.com>
2019-06-25 10:38:37 -0500

    updated the unit test cases

--- src/test/java/com/hexarch/controller/ItemControllerTest.java
+++ src/test/java/com/hexarch/controller/ItemControllerTest.java
@@ -1,6 +1,7 @@
 package com.hexarch.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hexarch.constants.ItemConstants;
 import com.hexarch.domain.Item;
 import com.hexarch.service.ItemService;
 import org.junit.Before;
@@ -13,14 +14,16 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
+import org.springframework.web.util.NestedServletException;
+import org.springframework.web.util.UriComponentsBuilder;
+import java.net.URI;
 import static com.hexarch.constants.ItemConstants.ITEM_URL;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -51,5 +54,64 @@ public class ItemControllerTest {
         mockMvc.perform(post((ITEM_URL)).content(itemBody).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
 
+    }
+
+    @Test
+    public void updateItem() throws Exception {
+        Item item = new Item(100,"SKU001", "Iphone XR", 999.99);
+        String itemBody = new ObjectMapper().writeValueAsString(item);
+        when(itemService.updateItem(any())).thenReturn(item);
+
+        mockMvc.perform(put((ITEM_URL)).content(itemBody).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void retrieveItem() throws Exception {
+
+        //given
+        Item item = new Item(201,"SKU001", "Iphone XR", 999.99);
+        Integer itemId = 201;
+        URI uri = UriComponentsBuilder.fromUriString(ItemConstants.SINGLE_ITEM).build(itemId);
+        when(itemService.retrieveItem(any())).thenReturn(item);
+
+        //when
+        String reponse = mockMvc.perform(get((uri)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+        .andReturn().getResponse().getContentAsString();
+
+        //then
+        Item responseItem = new ObjectMapper().readValue(reponse, Item.class);
+        assertEquals(itemId,responseItem.getId());
+
+    }
+
+    @Test
+    public void deleteItem() throws Exception {
+
+        //given
+        Integer itemId = 201;
+        URI uri = UriComponentsBuilder.fromUriString(ItemConstants.SINGLE_ITEM).build(itemId);
+
+        //when
+        mockMvc.perform(delete((uri)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        //then
+        verify(itemService,times(1)).deleteItem(any());
+    }
+
+    @Test(expected = NestedServletException.class)
+    public void saveItem_RuntimeException() throws Exception {
+
+        //given
+        Item item = new Item(100,"SKU001", "Iphone XR", 999.99);
+        String itemBody = new ObjectMapper().writeValueAsString(item);
+        when(itemService.saveItem(any())).thenThrow(new RuntimeException("ExceptionOccurred"));
+
+        //then
+        mockMvc.perform(post((ITEM_URL)).content(itemBody).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isInternalServerError());
     }
 }
